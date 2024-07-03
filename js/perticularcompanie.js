230f65271392cfb1deb0d0e9446923628a7f00c6
AkashSaha2001 <mr.akashsaha574@gmail.com>
2024-07-03 20:25:05 +0530

    files add

--- js/perticularcompanie.js
+++ js/perticularcompanie.js
@@ -23,74 +23,172 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
-// -------------------------------content change------------------------------
-function overview(){
-    let overview=document.getElementById("overview");
-    overview.style.display="block";
+// ------------------------------- Accenture content change------------------------------
+function AccentureOverview(){
+    let Accentureoverview=document.getElementById("Accentureoverview");
+    Accentureoverview.style.display="block";
 
-    let Cognative=document.getElementById("Cognative");
-    Cognative.style.display="none";
-    let Technical=document.getElementById("Technical");
-    Technical.style.display="none";
-    let Coding=document.getElementById("Coding");
-    Coding.style.display="none";
-    let Communication=document.getElementById("Communication");
-    Communication.style.display="none";
+    let AccentureCognitive=document.getElementById("AccentureCognitive");
+    AccentureCognitive.style.display="none";
+    let AccentureTechnical=document.getElementById("AccentureTechnical");
+    AccentureTechnical.style.display="none";
+    let AccentureCoding=document.getElementById("AccentureCoding");
+    AccentureCoding.style.display="none";
+    let AccentureCommunication=document.getElementById("AccentureCommunication");
+    AccentureCommunication.style.display="none";
 }
 
-function Cognative(){
-    let Cognative=document.getElementById("Cognative");
-    Cognative.style.display="block";
+function AccentureCognitive(){
+    let Cognitive=document.getElementById("AccentureCognitive");
+    Cognitive.style.display="block";
 
-    let overview=document.getElementById("overview");
-    overview.style.display="none";
-    let Technical=document.getElementById("Technical");
-    Technical.style.display="none";
-    let Coding=document.getElementById("Coding");
-    Coding.style.display="none";
-    let Communication=document.getElementById("Communication");
-    Communication.style.display="none";
+    let Accentureoverview=document.getElementById("Accentureoverview");
+    Accentureoverview.style.display="none";
+    let AccentureTechnical=document.getElementById("AccentureTechnical");
+    AccentureTechnical.style.display="none";
+    let AccentureCoding=document.getElementById("AccentureCoding");
+    AccentureCoding.style.display="none";
+    let AccentureCommunication=document.getElementById("AccentureCommunication");
+    AccentureCommunication.style.display="none";
 }
 
-function Technical(){
-    let Technical=document.getElementById("Technical");
-    Technical.style.display="block";
+function AccentureTechnical(){
+    let AccentureTechnical=document.getElementById("AccentureTechnical");
+    AccentureTechnical.style.display="block";
 
-    let overview=document.getElementById("overview");
-    overview.style.display="none";
-    let Cognative=document.getElementById("Cognative");
-    Cognative.style.display="none";
-    let Coding=document.getElementById("Coding");
-    Coding.style.display="none";
-    let Communication=document.getElementById("Communication");
-    Communication.style.display="none";
+    let Accentureoverview=document.getElementById("Accentureoverview");
+    Accentureoverview.style.display="none";
+    let AccentureCognitive=document.getElementById("AccentureCognitive");
+    AccentureCognitive.style.display="none";
+    let AccentureCoding=document.getElementById("AccentureCoding");
+    AccentureCoding.style.display="none";
+    let AccentureCommunication=document.getElementById("AccentureCommunication");
+    AccentureCommunication.style.display="none";
 }
 
 
-function Coding(){
-    let Coding=document.getElementById("Coding");
-    Coding.style.display="block";
+function AccentureCoding(){
+    let AccentureCoding=document.getElementById("AccentureCoding");
+    AccentureCoding.style.display="block";
 
-    let overview=document.getElementById("overview");
-    overview.style.display="none";
-    let Cognative=document.getElementById("Cognative");
-    Cognative.style.display="none";
-    let Technical=document.getElementById("Technical");
-    Technical.style.display="none";
-    let Communication=document.getElementById("Communication");
-    Communication.style.display="none";
+    let Accentureoverview=document.getElementById("Accentureoverview");
+    Accentureoverview.style.display="none";
+    let AccentureCognitive=document.getElementById("AccentureCognitive");
+    AccentureCognitive.style.display="none";
+    let AccentureTechnical=document.getElementById("AccentureTechnical");
+    AccentureTechnical.style.display="none";
+    let AccentureCommunication=document.getElementById("AccentureCommunication");
+    AccentureCommunication.style.display="none";
 }
 
-function Communication(){
-    let Communication=document.getElementById("Communication");
-    Communication.style.display="block";
+function AccentureCommunication(){
+    let AccentureCommunication=document.getElementById("AccentureCommunication");
+    AccentureCommunication.style.display="block";
 
-    let overview=document.getElementById("overview");
-    overview.style.display="none";
-    let Cognative=document.getElementById("Cognative");
-    Cognative.style.display="none";
-    let Technical=document.getElementById("Technical");
-    Technical.style.display="none";
-    let Coding=document.getElementById("Coding");
-    Coding.style.display="none";
+    let Accentureoverview=document.getElementById("Accentureoverview");
+    Accentureoverview.style.display="none";
+    let AccentureCognitive=document.getElementById("AccentureCognitive");
+    AccentureCognitive.style.display="none";
+    let AccentureTechnical=document.getElementById("AccentureTechnical");
+    AccentureTechnical.style.display="none";
+    let AccentureCoding=document.getElementById("AccentureCoding");
+    AccentureCoding.style.display="none";
+}
+
+
+// ------------------------------- Deloitte content change------------------------------
+function DeloitteOverview(){
+    let DeloitteOverview = document.getElementById("DeloitteOverview");
+    DeloitteOverview.style.display = "block";
+
+    let DeloitteQuants = document.getElementById("DeloitteQuants");
+    DeloitteQuants.style.display = "none";
+    let DeloitteVerbal = document.getElementById("DeloitteVerbal");
+    DeloitteVerbal.style.display = "none";
+    let DeloitteLogical = document.getElementById("DeloitteLogical");
+    DeloitteLogical.style.display = "none";
+    let DeloitteTechnical = document.getElementById("DeloitteTechnical");
+    DeloitteTechnical.style.display = "none";
+    let DeloitteCoding = document.getElementById("DeloitteCoding");
+    DeloitteCoding.style.display = "none";
+}
+
+function DeloitteQuants(){
+    let DeloitteQuants = document.getElementById("DeloitteQuants");
+    DeloitteQuants.style.display = "block";
+
+    let DeloitteOverview = document.getElementById("DeloitteOverview");
+    DeloitteOverview.style.display = "none";
+    let DeloitteVerbal = document.getElementById("DeloitteVerbal");
+    DeloitteVerbal.style.display = "none";
+    let DeloitteLogical = document.getElementById("DeloitteLogical");
+    DeloitteLogical.style.display = "none";
+    let DeloitteTechnical = document.getElementById("DeloitteTechnical");
+    DeloitteTechnical.style.display = "none";
+    let DeloitteCoding = document.getElementById("DeloitteCoding");
+    DeloitteCoding.style.display = "none";
+}
+
+function DeloitteVerbal(){
+    let DeloitteVerbal = document.getElementById("DeloitteVerbal");
+    DeloitteVerbal.style.display = "block";
+
+    let DeloitteOverview = document.getElementById("DeloitteOverview");
+    DeloitteOverview.style.display = "none";
+    let DeloitteQuants = document.getElementById("DeloitteQuants");
+    DeloitteQuants.style.display = "none";
+    let DeloitteLogical = document.getElementById("DeloitteLogical");
+    DeloitteLogical.style.display = "none";
+    let DeloitteTechnical = document.getElementById("DeloitteTechnical");
+    DeloitteTechnical.style.display = "none";
+    let DeloitteCoding = document.getElementById("DeloitteCoding");
+    DeloitteCoding.style.display = "none";
+}
+
+function DeloitteLogical(){
+    let DeloitteLogical = document.getElementById("DeloitteLogical");
+    DeloitteLogical.style.display = "block";
+
+    let DeloitteOverview = document.getElementById("DeloitteOverview");
+    DeloitteOverview.style.display = "none";
+    let DeloitteQuants = document.getElementById("DeloitteQuants");
+    DeloitteQuants.style.display = "none";
+    let DeloitteVerbal = document.getElementById("DeloitteVerbal");
+    DeloitteVerbal.style.display = "none";
+    let DeloitteTechnical = document.getElementById("DeloitteTechnical");
+    DeloitteTechnical.style.display = "none";
+    let DeloitteCoding = document.getElementById("DeloitteCoding");
+    DeloitteCoding.style.display = "none";
+}
+
+function DeloitteTechnical(){
+    let DeloitteTechnical = document.getElementById("DeloitteTechnical");
+    DeloitteTechnical.style.display = "block";
+
+    let DeloitteOverview = document.getElementById("DeloitteOverview");
+    DeloitteOverview.style.display = "none";
+    let DeloitteQuants = document.getElementById("DeloitteQuants");
+    DeloitteQuants.style.display = "none";
+    let DeloitteVerbal = document.getElementById("DeloitteVerbal");
+    DeloitteVerbal.style.display = "none";
+    let DeloitteLogical = document.getElementById("DeloitteLogical");
+    DeloitteLogical.style.display = "none";
+    let DeloitteCoding = document.getElementById("DeloitteCoding");
+    DeloitteCoding.style.display = "none";
+}
+
+function DeloitteCoding(){
+    let DeloitteCoding = document.getElementById("DeloitteCoding");
+    DeloitteCoding.style.display = "block";
+
+    let DeloitteOverview = document.getElementById("DeloitteOverview");
+    DeloitteOverview.style.display = "none";
+    let DeloitteQuants = document.getElementById("DeloitteQuants");
+    DeloitteQuants.style.display = "none";
+    let DeloitteVerbal = document.getElementById("DeloitteVerbal");
+    DeloitteVerbal.style.display = "none";
+    let DeloitteLogical = document.getElementById("DeloitteLogical");
+    DeloitteLogical.style.display = "none";
+    let DeloitteTechnical = document.getElementById("DeloitteTechnical");
+    DeloitteTechnical.style.display = "none";
 }
